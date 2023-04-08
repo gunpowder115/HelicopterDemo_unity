97a23e8f08fcdbd4972eff87c338c2c2356ee953
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2023-04-08 20:08:49 +0300

    Removed FPV dependency (added Time.deltaTime)
    Code refactoring
    Added serialized fields

--- HelicopterDemo/Assets/Scripts/Helicopter.cs
+++ HelicopterDemo/Assets/Scripts/Helicopter.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Helicopter : MonoBehaviour
 {
-    const float deltaRotationAngle = 1.0f;
-    const float stabilizationKoef = 0.2f;
+    [SerializeField] float rotSpeed = 100.0f;
+    [SerializeField] float takeSpeed = 800.0f;
+    [SerializeField] float stabilizeDeltaAngle = 1.0f;
+    [SerializeField] float stabilizationKoef = 0.2f;
     Rigidbody rigidBody;
-    float currentPitchRotation;
-    float currentRollRotation;
-    float currentYawRotation;
+    Vector3 vectorPitchRotation, vectorRollRotation, vectorYawRotation;
+    float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -27,81 +28,118 @@ public class Helicopter : MonoBehaviour
     void SimpleTakeoff()
     {
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
-    }
-
-    void SimplePitch()
-    {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-        {
-            currentPitchRotation = deltaRotationAngle;
-            transform.Rotate(new Vector3(currentPitchRotation, 0, 0));
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            currentPitchRotation = -deltaRotationAngle;
-            transform.Rotate(new Vector3(currentPitchRotation, 0, 0));
-        }
-        else
-        {
-            if (transform.localEulerAngles.x > deltaRotationAngle && transform.localEulerAngles.x < 360 - deltaRotationAngle)
-                transform.Rotate(new Vector3(-currentPitchRotation, 0, 0) * stabilizationKoef);
-            else
-            {
-                currentPitchRotation = 0;
-                transform.localEulerAngles = new Vector3(currentPitchRotation, transform.localEulerAngles.y, transform.localEulerAngles.z);
-            }
-
-        }
-    }
-
-    void SimpleRoll()
-    {
-        print(transform.localEulerAngles.z);
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            currentRollRotation = deltaRotationAngle;
-            transform.Rotate(new Vector3(0, 0, currentRollRotation));
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            currentRollRotation = -deltaRotationAngle;
-            transform.Rotate(new Vector3(0, 0, currentRollRotation));
-        }
-        else
-        {
-            if (transform.localEulerAngles.z > deltaRotationAngle && transform.localEulerAngles.z < 360 - deltaRotationAngle)
-                transform.Rotate(new Vector3(0, 0, -currentRollRotation) * stabilizationKoef);
-            else
-            {
-                currentRollRotation = 0;
-                transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, currentRollRotation);
-            }
-        }
-    }
-
-    void SimpleYaw()
-    {
-        if (Input.GetKey(KeyCode.Q))
-        {
-            currentYawRotation = -deltaRotationAngle;
-            transform.Rotate(new Vector3(0, currentYawRotation, 0));
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            currentYawRotation = deltaRotationAngle;
-            transform.Rotate(new Vector3(0, currentYawRotation, 0));
-        }
+            rigidBody.AddRelativeForce(Vector3.up * takeSpeed * Time.deltaTime);
     }
 
     void SimpleRotation()
     {
+        rotationSpeed = rotSpeed * Time.deltaTime;
+
         rigidBody.freezeRotation = true;
-        SimplePitch();
-        SimpleRoll();
-        SimpleYaw();
+        SimplePitchRotation();
+        SimpleRollRotation();
+        SimpleYawRotation();
         rigidBody.freezeRotation = false;
+    }
+
+    void SimplePitchRotation()
+    {
+        print("X angle = " + transform.localEulerAngles.x);
+
+        bool pitchUp = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        bool pitchDown = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (pitchUp || pitchDown)
+            Rotation(LocalAxes.Pitch, pitchDown, pitchUp);
+        else
+            Stabilization(LocalAxes.Pitch);
+    }
+
+    void SimpleRollRotation()
+    {
+        print("Z angle = " + transform.localEulerAngles.z);
+
+        bool rollLeft = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        bool rollRight = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+
+        if (rollLeft || rollRight)
+            Rotation(LocalAxes.Roll, rollLeft, rollRight);
+        else
+            Stabilization(LocalAxes.Roll);
+    }
+
+    void SimpleYawRotation()
+    {
+        print("Y angle = " + transform.localEulerAngles.y);
+
+        bool yawLeft = Input.GetKey(KeyCode.Q);
+        bool yawRight = Input.GetKey(KeyCode.E);
+
+        if (yawLeft || yawRight)
+            Rotation(LocalAxes.Yaw, yawLeft, yawRight);
+    }
+
+    void Rotation(LocalAxes localAxis, bool counterClockwise, bool clockwise)
+    {
+        Vector3 vectorRotation = new Vector3(0, 0, 0);
+        switch(localAxis)
+        {
+            case LocalAxes.Pitch:
+                if (counterClockwise && !clockwise)
+                    vectorPitchRotation = Vector3.right;
+                else if (clockwise && !counterClockwise)
+                    vectorPitchRotation = -Vector3.right;
+                vectorRotation = vectorPitchRotation;
+                break;
+            case LocalAxes.Roll:
+                if (counterClockwise && !clockwise)
+                    vectorRollRotation = Vector3.forward;
+                else if (clockwise && !counterClockwise)
+                    vectorRollRotation = -Vector3.forward;
+                vectorRotation = vectorRollRotation;
+                break;
+            case LocalAxes.Yaw:
+                if (counterClockwise && !clockwise)
+                    vectorYawRotation = -Vector3.up;
+                else if (clockwise && !counterClockwise)
+                    vectorYawRotation = Vector3.up;
+                vectorRotation = vectorYawRotation;
+                break;
+            default: break;
+        }
+        transform.Rotate(vectorRotation * rotationSpeed);
+    }
+
+    void Stabilization(LocalAxes localAxis)
+    {
+        float localEulerAngle = 0;
+        Vector3 stabVectorRotation = new Vector3(0, 0, 0);
+        Vector3 newLocalEuler = new Vector3(0, 0, 0);
+        switch(localAxis)
+        {
+            case LocalAxes.Pitch:
+                localEulerAngle = transform.localEulerAngles.x;
+                stabVectorRotation = -vectorPitchRotation;
+                newLocalEuler = new Vector3(0, transform.localEulerAngles.y, transform.localEulerAngles.z);
+                break;
+            case LocalAxes.Roll:
+                localEulerAngle = transform.localEulerAngles.z;
+                stabVectorRotation = -vectorRollRotation;
+                newLocalEuler = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
+                break;
+            default: break;
+        }
+
+        if (localEulerAngle > stabilizeDeltaAngle && localEulerAngle < 360 - stabilizeDeltaAngle)
+            transform.Rotate(stabVectorRotation * rotationSpeed * stabilizationKoef);
+        else
+            transform.localEulerAngles = newLocalEuler;
+    }
+
+    enum LocalAxes
+    {
+        Pitch,
+        Roll,
+        Yaw
     }
 }
