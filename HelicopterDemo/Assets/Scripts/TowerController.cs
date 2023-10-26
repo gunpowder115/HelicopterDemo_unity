f8fb78ba15b58a6e26c6be97805fbfbb48ce5e51
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2023-10-26 23:46:17 +0300

    Added TowerController
    for random creating towers in level

---
+++ HelicopterDemo/Assets/Scripts/TowerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    [SerializeField] int towerCount = 8;
+    [SerializeField] float minDistance = 100.0f;
+    [SerializeField] float maxDistance = 300.0f;
+    [SerializeField] GameObject towerPrefab;
+
+    private GameObject[] towers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        towers = new GameObject[towerCount];
+        float deltaAngle = 360 / towerCount;
+
+        for (int i = 0; i < towerCount; i++)
+        {
+            towers[i] = Instantiate(towerPrefab);
+
+            float towerDistance = Random.Range(minDistance, maxDistance);
+            towers[i].transform.position = new Vector3(0, 0, 0);
+            towers[i].transform.Rotate(0, deltaAngle * i, 0);
+            towers[i].transform.Translate(towerDistance, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
