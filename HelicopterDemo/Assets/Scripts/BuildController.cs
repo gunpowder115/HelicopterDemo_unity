5f0888c918a42b8041374335c9ac54e69db9f304
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2023-10-29 14:29:33 +0300

    BuildController modified
    it creates builds on each platform of tower

--- HelicopterDemo/Assets/Scripts/BuildController.cs
+++ HelicopterDemo/Assets/Scripts/BuildController.cs
@@ -17,13 +17,17 @@ public class BuildController : MonoBehaviour
     private GameObject[] builds;
     private GameObject[] platforms;
     private List<GameObject> prefabs;
+    private List<int> unusedPlatforms;
 
     // Start is called before the first frame update
     void Start()
     {
-        int buildCount = Random.Range(minBuildCount, maxBuildCount);
+        int buildCount = Random.Range(minBuildCount, maxBuildCount + 1);
         builds = new GameObject[buildCount];
         platforms = new GameObject[platformCount];
+        unusedPlatforms = new List<int>();
+        for (int i = 0; i < platformCount; i++)
+            unusedPlatforms.Add(i);
 
         float deltaAngle = 360 / platformCount;
         int prefabCount = AddPrefabs();
@@ -32,19 +36,23 @@ public class BuildController : MonoBehaviour
         {
             platforms[i] = Instantiate(platformPrefab);
 
+            platforms[i].transform.Rotate(0, deltaAngle * i + this.transform.position.y, 0);
+
             float platformDistance = Random.Range(minDistance, maxDistance);
             platforms[i].transform.position = new Vector3();
-            platforms[i].transform.Rotate(0, deltaAngle * i, 0);
             platforms[i].transform.Translate(platformDistance, 0, 0);
+            platforms[i].transform.position += this.transform.position;
         }
 
         for (int i = 0; i < buildCount; i++)
         {
-            builds[i] = Instantiate(prefabs[Random.Range(0, prefabCount - 1)]);
+            builds[i] = Instantiate(prefabs[Random.Range(0, prefabCount)]);
 
-            int platformIndex = Random.Range(0, platformCount);
+            int unusedPlatformIndex = Random.Range(0, unusedPlatforms.Count);
+            int platformIndex = unusedPlatforms[unusedPlatformIndex];            
             builds[i].transform.position = platforms[platformIndex].transform.position;
             builds[i].transform.rotation = platforms[platformIndex].transform.rotation;
+            unusedPlatforms.Remove(platformIndex);
         }
     }
 
