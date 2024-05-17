ef517332175caab4882d76cbffd22337a6ebd1b1
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2024-05-17 23:43:29 +0300

    SCRIPTS REFACTORING - PROTO DELETED
    all scripts from Proto moved to Scripts

--- HelicopterDemo/Assets/Scripts/Movement/SimpleRotation.cs
+++ HelicopterDemo/Assets/Scripts/Movement/SimpleRotation.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleRotation : MonoBehaviour
@@ -7,12 +5,12 @@ public class SimpleRotation : MonoBehaviour
     [SerializeField] float delta = 10.0f;
     [SerializeField] Axes axis = Axes.X;
 
-    private Vector3 deltaRotation;
+    Vector3 deltaRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        deltaRotation = new Vector3(axis == Axes.X ? delta : 0, 
+        deltaRotation = new Vector3(axis == Axes.X ? delta : 0,
                                     axis == Axes.Y ? delta : 0,
                                     axis == Axes.Z ? delta : 0);
     }
