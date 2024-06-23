3e59ca65c268f0673a6bbeb24da02f12c8c6b2d1
gunpowder115 <87995472+gunpowder115@users.noreply.github.com>
2024-06-23 14:17:55 +0300

    Added aim holding on selected target

--- HelicopterDemo/Assets/Scripts/Player/Crosshair.cs
+++ HelicopterDemo/Assets/Scripts/Player/Crosshair.cs
@@ -5,14 +5,17 @@ public class Crosshair : MonoBehaviour
     [SerializeField] private float aimSpeed = 5f;
     [SerializeField] private float rayRadius = 1f;
     [SerializeField] private float maxDistance = 100f;
+    [SerializeField] private float targetHoldTime = 1f;
     [SerializeField] private GameObject aimItem;
     [SerializeField] private GameObject targetAimItem;
 
     public static Crosshair singleton { get; private set; }
     public Vector2 ToTargetSelection => toTargetSelection;
 
-    private Vector2 toTargetSelection;
+    private float currHoldTime;
+    private Vector2 toTargetSelection, targetScreenPos;
     private Camera mainCamera;
+    private GameObject selectedTarget;
 
     void Awake()
     {
@@ -28,34 +31,30 @@ public class Crosshair : MonoBehaviour
 
     public void Show()
     {
-        if (aimItem)
-        {
-            aimItem.SetActive(true);
-            aimItem.transform.position = new Vector3(mainCamera.pixelWidth / 2f, mainCamera.pixelHeight / 2f, 0f);
-        }
+        aimItem.SetActive(true);
+        aimItem.transform.position = new Vector3(mainCamera.pixelWidth / 2f, mainCamera.pixelHeight / 2f, 0f);
     }
 
     public void Hide()
     {
         aimItem.SetActive(false);
         targetAimItem.SetActive(false);
+        selectedTarget = null;
+        currHoldTime = 0f;
     }
 
     public void Translate(Vector2 direction)
     {
-        if (aimItem)
-        {
-            float aimX, aimY;
-            aimX = Limit(aimItem.transform.position.x + direction.x * aimSpeed, 0f, mainCamera.pixelWidth);
-            aimY = Limit(aimItem.transform.position.y + direction.y * aimSpeed, 0f, mainCamera.pixelHeight);
-            aimItem.transform.position = new Vector3(aimX, aimY, 0f);
+        float aimX, aimY;
+        aimX = Limit(aimItem.transform.position.x + direction.x * aimSpeed, 0f, mainCamera.pixelWidth);
+        aimY = Limit(aimItem.transform.position.y + direction.y * aimSpeed, 0f, mainCamera.pixelHeight);
+        aimItem.transform.position = new Vector3(aimX, aimY, 0f);
 
-            float cameraCoefX = 2f * aimX / mainCamera.pixelWidth - 1f;
-            float cameraCoefY = 2f * aimY / mainCamera.pixelHeight - 1f;
-            toTargetSelection = new Vector2(cameraCoefX, -cameraCoefY);
+        float cameraCoefX = 2f * aimX / mainCamera.pixelWidth - 1f;
+        float cameraCoefY = 2f * aimY / mainCamera.pixelHeight - 1f;
+        toTargetSelection = new Vector2(cameraCoefX, -cameraCoefY);
 
-            SetTargetAim();
-        }
+        SetTargetAim();
     }
 
     private void SetTargetAim()
@@ -68,20 +67,36 @@ public class Crosshair : MonoBehaviour
             var hitObject = hit.transform.gameObject;
             if (hitObject.GetComponent<SimpleNpc>())
             {
-                var screenPos = mainCamera.WorldToScreenPoint(hitObject.transform.position);
-                Ray rayCenter = mainCamera.ScreenPointToRay(screenPos);
+                targetScreenPos = mainCamera.WorldToScreenPoint(hitObject.transform.position);
+                Ray rayCenter = mainCamera.ScreenPointToRay(targetScreenPos);
                 Physics.Raycast(rayCenter, out RaycastHit hitCenter);
                 var hitCenterObject = hitCenter.transform.gameObject;
                 if (hitObject == hitCenterObject)
                 {
-                    targetAimItem.SetActive(true);
-                    targetAimItem.transform.position = new Vector3(screenPos.x, screenPos.y, targetAimItem.transform.position.z);
+                    currHoldTime = targetHoldTime;
+                    selectedTarget = hitObject;
                     hitEnemy = true;
                     break;
                 }
             }
         }
-        if (!hitEnemy) targetAimItem.SetActive(false);
+
+        if (!hitEnemy)
+        {
+            if ((currHoldTime -= Time.deltaTime) > 0f && selectedTarget)
+            {
+                targetScreenPos = mainCamera.WorldToScreenPoint(selectedTarget.transform.position);
+                targetAimItem.transform.position = new Vector3(targetScreenPos.x, targetScreenPos.y, targetAimItem.transform.position.z);
+                targetAimItem.SetActive(true);
+            }
+            else
+                targetAimItem.SetActive(false);
+        }
+        else
+        {
+            targetAimItem.transform.position = new Vector3(targetScreenPos.x, targetScreenPos.y, targetAimItem.transform.position.z);
+            targetAimItem.SetActive(true);
+        }
     }
 
     private float Limit(float value, float min, float max)
